3e62789c73d35122f349112fb46411df4095eed3
Hu Zhiming <zmhu@base.ctripsmartdns.com>
2023-09-20 14:18:41 +0800

    fix: check screen size

--- lib/util/functions.dart
+++ lib/util/functions.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 import 'package:flutter_tiktoken/flutter_tiktoken.dart';
+import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 
 enum DeviceType { small, wide }
 
@@ -8,9 +10,12 @@ DeviceType getDeviceType() {
   double ratio = PlatformDispatcher.instance.displays.first.devicePixelRatio;
   Size size = PlatformDispatcher.instance.displays.first.size;
   // print("ration $ratio , size: $size");
-  double width = size.width / ratio;
-  double height = size.height / ratio;
+  double width = size.width;
+  double height = size.height;
+  Logger logger = Get.find<Logger>();
   // print("width: $width , height: $height");
+  logger.d("screen size -> width: $width , height: $height");
+  logger.d("screen size -> width: ${size.width}, height: ${size.height}, ratio: $ratio");
   if (width > 1000.0 && height > 700.0) {
     return DeviceType.wide;
   }
