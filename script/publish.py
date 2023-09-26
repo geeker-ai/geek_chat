64d831324fe633caec3160fc3fdee27dc3487b39
zmhu <sammeyhu@gmail.com>
2023-09-26 19:02:58 +0800

    feat: upload to gist

--- script/publish.py
+++ script/publish.py
@@ -1,7 +1,8 @@
-from github import Github, Download
+from github import Github, InputFileContent
 from github import Auth
 from datetime import datetime, timezone
 import os
+import json
 
 def download_file(url, file_name , token):
     command = "curl -L " + \
@@ -9,7 +10,9 @@ def download_file(url, file_name , token):
         "-H 'Authorization: Bearer {}' ".format(token) + \
         "-H 'X-GitHub-Api-Version: 2022-11-28' " + \
         "-o '{}' ".format(file_name) + "{}".format(url)
-    print(command)
+    return do_command(command=command)
+
+def do_command(command):
     process = os.popen(command)
     output = process.read()
     process.close()
@@ -18,6 +21,7 @@ def download_file(url, file_name , token):
 
 if __name__ == '__main__':
     github_token = os.environ.get("GEEK_CHAT_GITHUB_TOKEN")
+    base_dir ="./dist"
 
     if (github_token == None):
         exit(1)
@@ -36,10 +40,29 @@ if __name__ == '__main__':
 
     print("repo name: {}, tag_name: {} ".format(repo.name, tag_name))
 
-
-    save_dir = "./dist/{}".format(tag_name)
+    save_dir = "{}/{}".format(base_dir, tag_name)
     os.mkdir(save_dir)
 
     for asset in assets:
         print("aesst -> name: {}, id: {}, url: {}".format(asset.name, asset.id, asset.url))
-        download_file(asset.url, "{}/{}".format(save_dir, asset.name), github_token)
+        print(download_file(asset.url, "{}/{}".format(save_dir, asset.name), github_token))
+
+    r2dir = "r2:geekchat/{}".format(tag_name)
+    print(do_command("rclone mkdir {}}".format(r2dir)))
+    print(do_command("rclone copy -P {} {}".format(save_dir, r2dir)))
+
+
+    latest = {
+        "version": tag_name
+    }
+
+    latest_file = f"{base_dir}/latest.json"
+    with open(latest_file, 'w') as f:
+        f.write(json.dumps(latest))
+
+    print(do_command(f"rclone copy {latest_file} r2:geekchat"))
+
+    gist = g.get_gist("52a66708a59c6f7501c01bb43e3adcbd")
+    print(gist.files['latest.json'])
+    gist.edit(description="", files={'latest.json': InputFileContent(json.dumps(latest))})
+    gist.update()
