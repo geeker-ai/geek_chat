76263a4c03af519adbe9e6eb46f4527e5c7580d8
zmhu <sammeyhu@gmail.com>
2023-11-17 09:32:01 +0800

    feat: rm .env

---
+++ lib/util/tracker/geeker_tracker.dart
@@ -0,0 +1,9 @@
+import 'package:geek_chat/util/tracker/tracker_interface.dart';
+
+/*
+ * https://github.com/kentcb/azure_application_insights
+ * https://pub.dev/packages/azure_application_insights
+ */
+class GeekTracker extends TrackerInterface {
+  //
+}
