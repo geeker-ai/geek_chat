c3bb70d9817aa9ce35188eff3b68f03523ed89df
zmhu <sammeyhu@gmail.com>
2023-11-17 09:47:00 +0800

    ci: add insight key to env file

--- script/cmd.py
+++ script/cmd.py
@@ -2,6 +2,7 @@ import os
 import getopt, sys
 import yaml
 import dotenv
+import shutil
 
 def version_to_build_number(version:str):
     vlist = version.split(".")
@@ -21,12 +22,16 @@ def change_pubspec_version (version: str, build_number: int):
 def change_channel (value):
     dotenv.set_key(".env", "CHANNEL", value)
 
+def change_tracker_key (value):
+    dotenv.set_key(".env", "TRACKER_KEY", value)
+
 if __name__ == '__main__':
     version = os.environ.get("VERSION", "0.0.1")
     channel = os.environ.get("CHANNEL", "site")
+    tracker_key = os.environ.get("INSIGHT_KEY")
     argument_list = sys.argv[1:]
-    options = "vc"
-    long_options = ["verion", "channel"]
+    options = "vci"
+    long_options = ["verion", "channel", "init"]
     try:
         arguments, values = getopt.getopt(argument_list, options, long_options)
         for curren_arg, arg_value in arguments:
@@ -39,5 +44,9 @@ if __name__ == '__main__':
             elif curren_arg in ("-c", "--channel"):
                 change_channel(channel)
                 print(f"channel: {channel}")
+            elif curren_arg in ("-i", '--init'):
+                shutil.copyfile("env", ".env")
+                change_tracker_key(tracker_key)
+                print("init .env")
     except getopt.error as err:
         print(str(err))
